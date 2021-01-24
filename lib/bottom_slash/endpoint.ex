ba3f220b4fed02e300761647e8a0d33e5df917b2
Oliver Ni <oliver.ni@gmail.com>
2021-01-24 13:53:46 -0800

    Add ping endpoint

--- lib/bottom_slash/endpoint.ex
+++ lib/bottom_slash/endpoint.ex
@@ -7,6 +7,8 @@ defmodule BottomSlash.Endpoint do
 
   post "/", to: BottomSlash.Slash
 
+  get "/ping", do: conn |> send_resp(200, "Pong!")
+
   match _ do
     conn
     |> send_resp(404, "Page Not Found")

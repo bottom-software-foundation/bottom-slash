14605908a49b546b6c19d088a743e84acae14ed0
Oliver Ni <oliver.ni@gmail.com>
2021-01-25 08:29:19 -0800

    Fix allowed mentions

--- lib/bottom_slash/slash.ex
+++ lib/bottom_slash/slash.ex
@@ -4,13 +4,30 @@ defmodule BottomSlash.Slash do
   alias BottomSlash.Struct.InteractionApplicationCommandCallbackData, as: ResponseData
 
   command "ping" do
-    %InteractionResponse{type: 4, data: %ResponseData{content: "Pong!"}}
+    %InteractionResponse{
+      type: 4,
+      data: %ResponseData{
+        content: "Pong!",
+        allowed_mentions: %{
+          parse: ["users"]
+        }
+      }
+    }
   end
 
   command "bottomify" do
     {:ok, encoded} = Bottom.encode(List.first(interaction.data.options).value)
     name = "#{interaction.member.user.username}#{interaction.member.user.discriminator}"
-    %InteractionResponse{type: 3, data: %ResponseData{content: "**#{name}:** #{encoded}"}}
+
+    %InteractionResponse{
+      type: 3,
+      data: %ResponseData{
+        content: "**#{name}:** #{encoded}",
+        allowed_mentions: %{
+          parse: ["users"]
+        }
+      }
+    }
   end
 
   command "regress" do
@@ -20,6 +37,14 @@ defmodule BottomSlash.Slash do
         _ -> "Failed to decode bottom."
       end
 
-    %InteractionResponse{type: 4, data: %ResponseData{content: text}}
+    %InteractionResponse{
+      type: 4,
+      data: %ResponseData{
+        content: text,
+        allowed_mentions: %{
+          parse: ["users"]
+        }
+      }
+    }
   end
 end
